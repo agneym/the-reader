3f27970bb478d178271bf54b0a98a0c5a8b2049d
Agney <agney@outlook.in>
2020-01-04 08:35:42 +0530

    styled components setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react";
+import GlobalStyle from "./utils/Global";
+import { ThemeProvider } from "styled-components";
+import theme from "./utils/theme";
 
-const App: React.FC = () => {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
