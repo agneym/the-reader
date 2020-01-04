0b1e4bea5a689518ef37129f1f06e3d775ea087f
Agney <agney@outlook.in>
2020-01-04 19:02:44 +0530

    add manifest icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import styled from "styled-components";
+import { Twitter } from "react-feather";
 
 const Nav = styled.nav`
   width: 100%;
@@ -9,10 +10,28 @@ const Nav = styled.nav`
   z-index: 50;
   background-color: #ffffff;
   top: 0;
+  display: flex;
+  align-items: center;
+  padding: 0 1.5em;
+  justify-content: space-between;
 `;
 
 const Navbar: FC = () => {
-  return <Nav></Nav>;
+  return (
+    <Nav>
+      <p>The Reader</p>
+      <div>
+        <a
+          href="https://twitter.com/agneymenon"
+          title="Follow me on Twitter"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Twitter aria-label="Twitter" />
+        </a>
+      </div>
+    </Nav>
+  );
 };
 
 export default Navbar;
