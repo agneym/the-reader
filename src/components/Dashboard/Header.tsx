ec3f894d0fdeb0a6bd44b5493317e06b09a40aa8
Agney <agney@outlook.in>
2020-01-04 18:07:46 +0530

    put parsed url to input

--- src/components/Dashboard/Header.tsx
+++ src/components/Dashboard/Header.tsx
@@ -1,7 +1,8 @@
-import React, { FC, useState, FormEvent } from "react";
+import React, { FC, useState, FormEvent, useEffect } from "react";
 import styled from "styled-components";
 
 import AddURL from "./AddURL";
+import { useLocation } from "react-router";
 
 const Container = styled.header``;
 
@@ -11,7 +12,14 @@ interface IProps {
 }
 
 const Header: FC<IProps> = ({ loading, onView }) => {
+  const location = useLocation();
   const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const url = searchParams.get("q");
+    setUrl(url ?? "");
+  }, []);
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
