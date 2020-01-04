93e50bc4b6e955ec8dfa4d4986e1137400dc3cf2
Agney <agney@outlook.in>
2020-01-04 12:18:08 +0530

    setup spinner container

--- src/components/Dashboard/Content.tsx
+++ src/components/Dashboard/Content.tsx
@@ -2,28 +2,61 @@ import React, { FC, useState } from "react";
 import styled from "styled-components";
 
 import Header from "./Header";
+import Spinner from "../Spinner";
+
+const topSpacing = "3em";
 
 const Main = styled.main`
-  margin: calc(${props => props.theme.nav.height} + 3em) auto 0 auto;
-  background-color: #ffffff;
+  margin: 0 auto;
   width: 100%;
-  max-width: 800px;
-  padding: 2em 1em;
-  box-shadow: ${props => props.theme.boxShadows.xl};
+  max-width: 1000px;
+  padding-top: calc(${props => props.theme.nav.height} + ${topSpacing});
 `;
 
-const ViewArea = styled.section``;
+const Container = styled.div`
+  box-shadow: ${props => props.theme.boxShadows.xl};
+  background-color: #ffffff;
+  padding: 2em 1em;
+  min-height: calc(
+    100vh - ${props => props.theme.nav.height} - ${topSpacing} - 3em
+  );
+  display: flex;
+  flex-direction: column;
+`;
+
+const ViewArea = styled.section`
+  position: relative;
+  height: 100%;
+  flex: 1;
+`;
+
+const SpinnerContainer = styled.div`
+  height: 3em;
+  width: 3em;
+  color: ${props => props.theme.colors.primary};
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%);
+`;
 
 const Content: FC = () => {
   const [loading, setLoading] = useState(false);
   const handleView = (url: string) => {
     setLoading(true);
-    console.log(url);
   };
   return (
     <Main>
-      <Header loading={loading} onView={handleView} />
-      <ViewArea></ViewArea>
+      <Container>
+        <Header loading={loading} onView={handleView} />
+        <ViewArea>
+          {loading && (
+            <SpinnerContainer>
+              <Spinner />
+            </SpinnerContainer>
+          )}
+        </ViewArea>
+      </Container>
     </Main>
   );
 };
--- src/utils/Global.ts
+++ src/utils/Global.ts
@@ -1,13 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  html * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     background-color: ${props => props.theme.colors.gray300};
     font-family: ${props => props.theme.fonts.main};
     line-height: 1.5;
     font-size: 100%;
-    box-sizing: border-box;
     min-height: 100vh;
   }
   h1, h2, h3, h4, h5, h6 {
